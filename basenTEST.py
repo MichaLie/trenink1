a456edc257fda72adc52aa30b0b93db3877e0fd6
MichaelaLie <michaela.liegertova@gmail.com>
2023-04-06 18:43:41 +0200

    první commit

---
+++ basenTEST.py
@@ -0,0 +1,9 @@
+# prográmek na básničku kam lze zadávat libolná jména
+muzske_jmeno = input("Zadej libovolné mužské jméno: ")
+zenske_jmeno = input("Zadej libovolné ženské jméno: ")
+
+vers_muz = f"{muzske_jmeno} v lese dřevo seká"
+print(vers_muz)
+
+vers_zena = f"{zenske_jmeno} na něj doma čeká"
+print(vers_zena)
